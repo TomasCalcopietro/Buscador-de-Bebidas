ad06ee45c9d29154cef89eb12721a14b173a2834
TomasCalcopietro <tomascalcopietro0@gmail.com>
2022-10-12 19:18:23 -0300

    first commit

---
+++ src/components/Bebida.jsx
@@ -0,0 +1,39 @@
+import { Col, Card, Button } from "react-bootstrap"
+import useBebidas from "../hooks/useBebidas"
+
+const Bebida = ({bebida}) => {
+
+  const {handleModalClick, handleBebidaIdClick} = useBebidas()
+
+  return (
+    <Col md={6} lg={4}>
+        <Card className='mb-4'>
+          <Card.Img
+            variant="top"
+            src={bebida.strDrinkThumb}
+            alt={`Imagen de ${bebida.strDrink}`}
+          />
+
+            <Card.Title
+              className="text-center mt-2"
+            >
+              {bebida.strDrink}
+            </Card.Title>
+
+            <Button
+              variant={"warning"}
+              className="w-100 text-uppercase mt-2"
+              onClick={ () => {
+                handleModalClick()
+                handleBebidaIdClick(bebida.idDrink)
+              }}
+            >
+              Ver Receta
+            </Button>
+
+        </Card>
+    </Col>
+  )
+}
+
+export default Bebida
